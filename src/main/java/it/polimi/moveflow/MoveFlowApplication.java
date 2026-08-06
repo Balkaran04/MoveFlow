9c99e01641e68453c7acc4e2bf2c5d8b1ab25161
Balkaran04 <balkaransingh1082004@gmail.com>
2026-08-06 09:47:12 +0200

    Initial commit

---
+++ src/main/java/it/polimi/moveflow/MoveFlowApplication.java
@@ -0,0 +1,13 @@
+package it.polimi.moveflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/polimi/moveflow/MoveFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polimi.moveflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
